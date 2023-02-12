6c86cc2498b4cf2ee171fb91abd8a771656143f1
Mariana Beldi <mbeldi@gmail.com>
2023-02-12 20:26:49 -0300

    styles

---
+++ .nuxt/dist/server/components/sidebar-eyes.js
@@ -0,0 +1,93 @@
+exports.ids = [5];
+exports.modules = {
+
+/***/ 25:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/SidebarEyes.vue?vue&type=template&id=47604d46&scoped=true&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"side-box"},[_vm._ssrNode("<h3 data-v-47604d46>Eyes</h3> <div class=\"side-box-option\" data-v-47604d46><label for=\"eyeFill\" data-v-47604d46>Fill</label> <input type=\"color\" id=\"eyeFill\" name=\"eyeFill\""+(_vm._ssrAttr("value",(_vm.$state.colorValues.eyeFill)))+" data-v-47604d46></div> <div class=\"side-box-option\" data-v-47604d46><label for=\"eyeBorder\" data-v-47604d46>Border</label> <input type=\"color\" id=\"eyeBorder\" name=\"eyeBorder\""+(_vm._ssrAttr("value",(_vm.$state.colorValues.eyeBorder)))+" data-v-47604d46></div> <div class=\"side-box-option\" data-v-47604d46><label for=\"eyeBorderSize\" data-v-47604d46>Border Width</label> <input type=\"range\" id=\"eyeBorderSize\" name=\"eyeBorderSize\" min=\"0\" max=\"100\""+(_vm._ssrAttr("value",(_vm.$state.numberValues.eyeBorderSize.value)))+" data-v-47604d46></div> <div class=\"side-box-option\" data-v-47604d46><label for=\"eyeSize\" data-v-47604d46>Size</label> <input type=\"range\" id=\"eyeSize\" name=\"eyeSize\" min=\"0\" max=\"100\""+(_vm._ssrAttr("value",(_vm.$state.numberValues.eyeSize.value)))+" data-v-47604d46></div> <div class=\"side-box-option\" data-v-47604d46><label for=\"eyePositionLX\" data-v-47604d46>Position Left X</label> <input type=\"range\" id=\"eyePositionLX\" name=\"eyePositionLX\" min=\"-50\" max=\"150\""+(_vm._ssrAttr("value",(_vm.$state.numberValues.eyePositionLX.value)))+" data-v-47604d46></div> <div class=\"side-box-option\" data-v-47604d46><label for=\"eyePositionLY\" data-v-47604d46>Position Left Y</label> <input type=\"range\" id=\"eyePositionLY\" name=\"eyePositionLY\" min=\"-50\" max=\"150\""+(_vm._ssrAttr("value",(_vm.$state.numberValues.eyePositionLY.value)))+" data-v-47604d46></div> <div class=\"side-box-option\" data-v-47604d46><label for=\"eyePositionRX\" data-v-47604d46>Position Right X</label> <input type=\"range\" id=\"eyePositionRX\" name=\"eyePositionRX\" min=\"-50\" max=\"150\""+(_vm._ssrAttr("value",(_vm.$state.numberValues.eyePositionRX.value)))+" data-v-47604d46></div> <div class=\"side-box-option\" data-v-47604d46><label for=\"eyePositionRY\" data-v-47604d46>Position Right Y</label> <input type=\"range\" id=\"eyePositionRY\" name=\"eyePositionRY\" min=\"-50\" max=\"150\""+(_vm._ssrAttr("value",(_vm.$state.numberValues.eyePositionRY.value)))+" data-v-47604d46></div>")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./components/SidebarEyes.vue?vue&type=template&id=47604d46&scoped=true&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/SidebarEyes.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var SidebarEyesvue_type_script_lang_js_ = ({
+  methods: {}
+});
+// CONCATENATED MODULE: ./components/SidebarEyes.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_SidebarEyesvue_type_script_lang_js_ = (SidebarEyesvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./components/SidebarEyes.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  components_SidebarEyesvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  "47604d46",
+  "fd855286"
+  
+)
+
+/* harmony default export */ var SidebarEyes = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=sidebar-eyes.js.map
